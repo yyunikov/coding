bf7366fe921ea6bacbed6fbcdb2ad6e6697a9a5e
yyunikov <yy@vgs.io>
2020-11-27 12:41:21 +0200

    Removed encryption part

--- src/main/java/com/verygood/security/coding/CodingApplication.java
+++ src/main/java/com/verygood/security/coding/CodingApplication.java
@@ -3,6 +3,5 @@ package com.verygood.security.coding;
 public class CodingApplication {
 
 	public static void main(String[] args) {
-		// TODO encrypt passed data with algorithm of your choice
 	}
 }
--- src/main/java/com/verygood/security/coding/Encryption.java
+++
@@ -1,6 +0,0 @@
-package com.verygood.security.coding;
-
-public interface Encryption {
-  String encrypt(String text);
-  String decrypt(String encryptedData);
-}
