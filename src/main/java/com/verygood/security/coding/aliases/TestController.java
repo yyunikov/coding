55a7e3dd15cde4f4e6cbaf019686997c30b00e75
yyunikov <yy@vgs.io>
2020-11-27 12:49:00 +0200

    Empty API

---
+++ src/main/java/com/verygood/security/coding/aliases/TestController.java
@@ -0,0 +1,13 @@
+package com.verygood.security.coding.aliases;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+}
